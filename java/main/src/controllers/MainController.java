5ecd6e81bf32790d02e8bdf20bab90faa0f3a0ee
Jaro <jaroslav.kuchta@gmail.com>
2018-10-18 19:25:07 +0200

    new bean created

--- java/main/src/controllers/MainController.java
+++ java/main/src/controllers/MainController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 public class MainController {
@@ -9,5 +14,20 @@ public class MainController {
     @RequestMapping("/ ")
     public String index(){
            return "main controller";
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
